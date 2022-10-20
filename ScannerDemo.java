d2064a81876a80d20b72ac2d05d10b68aa749138
Sebastian Krüger <sebastiankrueger.berlin@outlook.de>
2022-10-20 10:48:17 +0200

    Scanner eingefügt

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -13,18 +13,23 @@ public class ScannerDemo {
 		Scanner tastatur = new Scanner(System.in); // Erzeugen eines Objekts der Klasse Scanner
 
 		System.out.print("Bitte geben Sie eine ganze Zahl ein: ");
+		ganzzahl = tastatur.nextInt();
 		System.out.print("Sie haben " + ganzzahl + " eingegeben!\n\n");
 
 		System.out.print("Bitte geben Sie eine Zahl mit Nachkommastellen ein: ");
+		kommazahl = tastatur.nextDouble();
 		System.out.print("Sie haben " + kommazahl + " eingegeben!\n\n");
 
 		System.out.print("Bitte geben Sie eine Wort ein : ");
+		wort = tastatur.next();
 		System.out.print("Sie haben '" + wort + "' eingegeben!\n\n");
 
 		System.out.print("Bitte geben Sie einen Wahrheitswert ein (true / false): ");
+		wahrheitswert = tastatur.nextBoolean();
 		System.out.print("Sie haben '" + wahrheitswert + "' eingegeben!\n\n");
 
 		System.out.print("Bitte geben Sie einen einzelnen Buchstaben ein: ");
+		buchstabe = tastatur.next().charAt(0);
 		System.out.print("Sie haben '" + buchstabe + "' eingegeben!\n");
 
 		tastatur.close();
